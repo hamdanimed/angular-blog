1a1655c0807d75a22e264537c8db0476a5e42327
hamzaelgarai <hamzaelgarai10@gmail.com>
2023-04-11 23:37:11 +0000

    header for the app (in progress)

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {faHome,faSignOut,faList,faSearch} from '@fortawesome/free-solid-svg-icons'
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  faHome=faHome;
+  faSignOut = faSignOut;
+  faList = faList;
+  faSearch = faSearch;
+
+}
