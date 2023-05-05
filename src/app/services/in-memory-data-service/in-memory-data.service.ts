56ac76380b459c32b717065aa58e3749c2f695c6
hamza-elgarai <hamzaelgarai10@gmail.com>
2023-05-05 19:34:55 +0100

    hidden scrollbar when not needed - reverting data

--- src/app/services/in-memory-data-service/in-memory-data.service.ts
+++ src/app/services/in-memory-data-service/in-memory-data.service.ts
@@ -69,15 +69,6 @@ export class InMemoryDataService implements InMemoryDataService{
       {id:5,userId:1,postId:2,date:faker.datatype.number({min:1681342105})},
       {id:6,userId:1,postId:3,date:faker.datatype.number({min:1681342105})},
       {id:7,userId:1,postId:5,date:faker.datatype.number({min:1681342105})},
-      {id:7,userId:1,postId:5,date:faker.datatype.number({min:1681342105})},
-      {id:7,userId:1,postId:5,date:faker.datatype.number({min:1681342105})},
-      {id:7,userId:1,postId:5,date:faker.datatype.number({min:1681342105})},
-      {id:7,userId:1,postId:5,date:faker.datatype.number({min:1681342105})},
-      {id:7,userId:1,postId:5,date:faker.datatype.number({min:1681342105})},
-      {id:7,userId:1,postId:5,date:faker.datatype.number({min:1681342105})},
-      {id:7,userId:1,postId:5,date:faker.datatype.number({min:1681342105})},
-      {id:7,userId:1,postId:5,date:faker.datatype.number({min:1681342105})},
-      {id:7,userId:1,postId:5,date:faker.datatype.number({min:1681342105})},
     ];
     const categories : Category[] =[
       {id:1,name:"sports"},
